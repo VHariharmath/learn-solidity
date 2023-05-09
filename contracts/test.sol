8ba344b7665e734af2ff6f7029d278fb34689e67
vhariharmath <vinayak.hariharmath@gmail.com>
2023-05-09 16:21:48 +0530

    learning

--- contracts/test.sol
+++ contracts/test.sol
@@ -1,1 +1,14 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    string public str = "Hello world";
+
+    function updateStr(string memory _updateString) public {
+        str = _updateString;
+    }
+
+    function getStr() view public returns (string memory) {
+        return str;
+    }
+}
