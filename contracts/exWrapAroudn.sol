ae3b31bb83deedc0b126ec3ad579d31b96599dd5
vhariharmath <vinayak.hariharmath@gmail.com>
2023-05-09 17:12:43 +0530

    learning

---
+++ contracts/exWrapAroudn.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyVars {
+    uint public myUint;
+    uint8 public myUint8 = 254;
+    int public myInt = -10;
+
+    function setMyVar(uint _myUint) public  {
+        myUint = _myUint;
+    }
+
+    function incUint8() public {
+        unchecked {
+            myUint8++;
+        }
+    }
+
+    function incInt() public {
+        myInt++;
+    }
+
+    function decUInt() public {
+        myUint--;
+    }
+}
